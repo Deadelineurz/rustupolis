5722c510879c4182c07ac92ac2b4fcdf9d16b0c3
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-04 22:58:59 +0100

    refactor: ui binary

--- src/bin/ui.rs
+++ src/bin/ui.rs
@@ -2,26 +2,34 @@ use std::error::Error;
 use std::io::{stdout, Stdout};
 use std::thread::sleep;
 use std::time::Duration;
-use rustupolis::terminal::screen::CleanScreen;
-use rustupolis::ui::bars::*;
+use rustupolis::terminal::screen::{set_background, CleanScreen};
+use rustupolis::ui::*;
+use rustupolis::ui::colors::*;
 use termion::*;
 
 fn main()  -> Result<(), Box<dyn Error>> {
 
     let mut stdout: Stdout = stdout();
+
+    set_background(UI_BLACK_LIGHT_COLOR);
     let _scr = CleanScreen::new();
 
-    loop {
+    // loop {
 
-        let size = terminal_size()?;
+    //     let size = terminal_size()?;
     
     
-        draw_ui(&mut stdout, size, None)?;
+    //     draw_ui(&mut stdout, size, None)?;
 
-        sleep(Duration::from_secs(1));
+    //     sleep(Duration::from_secs(1));
 
-    }
+    // }
 
+    let size = terminal_size()?;
+    
+    draw_ui(&mut stdout, size, None)?;
 
-    // Ok(())
+    sleep(Duration::from_secs(5));
+
+    Ok(())
 }
