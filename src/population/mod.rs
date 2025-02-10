329ed894dc14c7972c3381807d99d88670539654
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-10 22:24:38 +0100

    refactor: moved part to sub modules

--- src/population/mod.rs
+++ src/population/mod.rs
@@ -1,5 +1,10 @@
-use std::ops::{BitAnd, BitOr, BitXor, Not};
+pub mod disease;
+pub mod dna;
 
+use dna::*;
+use disease::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CauseOfDeath {
     OldAge,
     Murder,
@@ -9,6 +14,7 @@ pub enum CauseOfDeath {
     EatenByMonster,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Mood {
     Zealot, // see the Mayor as their god and so won't notice the state they are in.
     Happy,
@@ -16,12 +22,14 @@ pub enum Mood {
     Unhappy,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct BasePeople {
     pub age: u8,
-    pub peopletype: PeopleDNA,
+    pub peopletype: DNA
 }
 
-enum People {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum People {
     Alive {
         base: BasePeople,
         mood: Mood,
@@ -31,79 +39,4 @@ enum People {
         base: BasePeople,
         cause: CauseOfDeath,
     },
-}
-
-#[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum PeopleDNA {
-    None =              0,
-    Irradiated =        1,  // Will live less
-    Fragile =           2,  // More likely to catch a desease
-
-
-    Immortal =          4,  // Can not die of injury
-    Unaging =           8,  // Can not age
-    Immune =            16, // Can not become sick 
-    HealthyCarrier =    32, // Can NOT die from his own desease, but can spread it
-}
-
-impl BitAnd for PeopleDNA {
-    type Output = u8;
-
-    fn bitand(self, rhs: Self) -> Self::Output {
-        self as u8 & rhs as u8
-    }
-}
-
-impl BitOr for PeopleDNA {
-    type Output = u8;
-
-    fn bitor(self, rhs: Self) -> Self::Output {
-        self as u8 | rhs as u8
-    }
-}
-
-impl BitXor for PeopleDNA {
-    type Output = u8;
-
-    fn bitxor(self, rhs: Self) -> Self::Output {
-        self as u8 ^ rhs as u8
-    }
-}
-
-impl Not for PeopleDNA {
-    type Output = u8;
-
-    fn not(self) -> Self::Output {
-        !(self as u8)
-    }
-}
-
-// _____ Desease _____
-
-#[derive(Debug, Clone, Copy)]
-pub enum DiseaseName {
-    Bacteria, // should probably be real name and not diseases type
-              //...
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum DiseaseLethality {
-    Deadly,
-    Moderate,
-    Low,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum DiseaseContagionRate {
-    High,
-    Mederate,
-    Low,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Disease {
-    pub name: DiseaseName,
-    pub lethality: DiseaseLethality,
-    pub contagion: DiseaseContagionRate,
 }
