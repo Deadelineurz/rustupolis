a05fd03243a14af1f181077ee15a568d834de834
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-11 01:07:22 +0100

    refactor: updated population binary

--- src/bin/population.rs
+++ src/bin/population.rs
@@ -1,15 +1,22 @@
 use rustupolis::population::dna::*;
+use rustupolis::population::people::*;
 use rustupolis::population::*;
+use rustupolis::population::district::*;
 
 fn main() {
     let kevin = People::Alive {
         base: BasePeople {
             age: 30,
-            peopletype: DNA::from_flags(DNAFlags::StrongPhysique | DNAFlags::Aggressive)
+            peopletype: DNA::from_flags(DNAFlags::StrongPhysique | DNAFlags::Aggressive),
         },
         mood: Mood::Neutral,
         disease: None,
     };
 
     dbg!(kevin);
+
+    let mut population = Population::new();
+
+    let _district =
+        PopulationDistrict::instantiate(vec![kevin], DistrictZone::Residentials, &mut population);
 }
