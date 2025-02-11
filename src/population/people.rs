0085031729f9c791ce5d348d30d3b18464686d89
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-11 22:21:15 +0100

    feat(people): added fn to create random peoples

--- src/population/people.rs
+++ src/population/people.rs
@@ -1,5 +1,7 @@
-use crate::population::dna::*;
 use crate::population::disease::*;
+use crate::population::dna::*;
+use rand::random_range;
+use strum::EnumCount;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CauseOfDeath {
@@ -17,13 +19,13 @@ pub enum Mood {
     Happy = 1,
     Neutral = 0,
     Unhappy = -1,
-    Angry = -2
+    Angry = -2,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct BasePeople {
     pub age: u8,
-    pub peopletype: DNA
+    pub dna: DNA,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -37,4 +39,38 @@ pub enum People {
         base: BasePeople,
         cause: CauseOfDeath,
     },
+}
+
+impl People {
+    /// Create a new alive people, will add a random number of DNA traits
+    pub fn create_random_people(working_age: bool, max_dna_traits: u8) -> Self {
+        let age = match working_age {
+            true => 18 + rand::random_range(0..=17),
+            false => rand::random_range(0..18),
+        };
+
+        let mut dna_traits: u32 = 0;
+        for _ in 0..max_dna_traits {
+            dna_traits |= 2_u32.pow(random_range(0_u32..DNAFlags::COUNT as u32));
+        }
+
+        People::Alive {
+            base: BasePeople {
+                age,
+                dna: DNA::from_flags(dna_traits),
+            },
+            mood: Mood::Neutral,
+            disease: None,
+        }
+    }
+
+    /// Create new (alive) peoples with 1 DNA trait each.
+    pub fn create_random_population(amount : u8) -> Vec<People> {
+        let mut peoples = vec![];
+        for _ in 0..amount {
+            peoples.push(People::create_random_people(true, 1));
+        }
+
+        peoples
+    }
 }
