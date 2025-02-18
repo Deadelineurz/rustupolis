7310bcfde3d59a00286489a16a4b9896e4ddde02
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-18 20:54:22 +0100

    feat(ui): sidebar is kinda working

--- src/bin/ui.rs
+++ src/bin/ui.rs
@@ -1,35 +1,70 @@
+use rustupolis::terminal::screen::{set_background, CleanScreen};
+use rustupolis::ui::colors::*;
+use rustupolis::ui::sidebar::*;
 use std::error::Error;
 use std::io::{stdout, Stdout};
 use std::thread::sleep;
 use std::time::Duration;
-use rustupolis::terminal::screen::{set_background, CleanScreen};
-use rustupolis::ui::*;
-use rustupolis::ui::colors::*;
-use termion::*;
 
-fn main()  -> Result<(), Box<dyn Error>> {
-
+fn main() -> Result<(), Box<dyn Error>> {
     let mut stdout: Stdout = stdout();
 
     set_background(UI_BLACK_LIGHT_COLOR);
     let _scr = CleanScreen::new();
 
-    // loop {
+    let mut side_bar = SideBar::new();
 
-    //     let size = terminal_size()?;
-    
-    
-    //     draw_ui(&mut stdout, size, None)?;
+    side_bar.draw(&mut stdout)?;
 
-    //     sleep(Duration::from_secs(1));
+    for i in 0..side_bar.get_max_number_of_logs() {
+        side_bar.push_log(
+            vec![Box::new("test ".to_string() + &i.to_string())],
+            rustupolis::ui::sidebar::LogType::Debug,
+            rustupolis::ui::sidebar::LogColor::Normal,
+        );
 
-    // }
+        sleep(Duration::from_millis(50));
 
-    let size = terminal_size()?;
-    
-    draw_ui(&mut stdout, size, None)?;
+        side_bar.draw_logs(&mut stdout)?;
+    }
 
-    sleep(Duration::from_secs(5));
+    side_bar.display_custom_infos(
+        &mut stdout,
+        &"Building info:",
+        &[
+            &"Name = Trump Tower",
+            &"ID = 3",
+            &"Residents = 63",
+            &"Size = ginormous",
+            &"Girth = huge",
+            &"Wow factor = WOooOooOOoW!!!",
+        ],
+    )?;
+
+    sleep(Duration::from_secs(1));
+
+    side_bar.push_log_and_display(
+        &mut stdout,
+        vec![
+            Box::new("A problem as occured..."),
+            Box::new("no it's a joke everything is fine."),
+        ],
+        LogType::Debug,
+        LogColor::Important,
+    )?;
+    sleep(Duration::from_secs(1));
+
+    side_bar.push_log_and_display(
+        &mut stdout,
+        vec![Box::new("or is it ?")],
+        LogType::Debug,
+        LogColor::Unusual,
+    )?;
+    sleep(Duration::from_secs(1));
+
+    side_bar.clear_custom_infos(&mut stdout)?;
+
+    sleep(Duration::from_secs(1));
 
     Ok(())
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,16 +6,16 @@ pub mod bottombar;
 
 pub mod colors;
 
-pub fn draw_ui(
-    stdout: &mut Stdout,
-    terminal_size: (u16, u16),
-    side_bar_size: Option<u16>,
-) -> Result<(), Error> {
-    let side_bar_size = side_bar_size.unwrap_or(terminal_size.0 / 4);
+// pub fn draw_ui(
+//     stdout: &mut Stdout,
+//     terminal_size: (u16, u16),
+//     side_bar_size: Option<u16>,
+// ) -> Result<(), Error> {
+//     let side_bar_size = side_bar_size.unwrap_or(terminal_size.0 / 4);
     
-    sidebar::draw_sidebar(stdout, terminal_size, side_bar_size)?;
-    topbar::draw_topbar(stdout, terminal_size, side_bar_size)?;
-    bottombar::draw_bottombar(stdout, terminal_size, side_bar_size)?;
+//     sidebar::draw_sidebar(stdout, terminal_size, side_bar_size)?;
+//     topbar::draw_topbar(stdout, terminal_size, side_bar_size)?;
+//     bottombar::draw_bottombar(stdout, terminal_size, side_bar_size)?;
 
-    Ok(())
-}
+//     Ok(())
+// }
