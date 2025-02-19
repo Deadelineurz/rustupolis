3efc481e64e76848a10598c9a174233ea723becf
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-19 11:23:30 +0100

    refactor(ui): cleaning mod.rs

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,21 +1,5 @@
-use std::io::{Error, Stdout};
-
 pub mod topbar;
 pub mod sidebar;
 pub mod bottombar;
 
 pub mod colors;
-
-// pub fn draw_ui(
-//     stdout: &mut Stdout,
-//     terminal_size: (u16, u16),
-//     side_bar_size: Option<u16>,
-// ) -> Result<(), Error> {
-//     let side_bar_size = side_bar_size.unwrap_or(terminal_size.0 / 4);
-    
-//     sidebar::draw_sidebar(stdout, terminal_size, side_bar_size)?;
-//     topbar::draw_topbar(stdout, terminal_size, side_bar_size)?;
-//     bottombar::draw_bottombar(stdout, terminal_size, side_bar_size)?;
-
-//     Ok(())
-// }
