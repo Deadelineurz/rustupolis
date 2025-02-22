478be6081e335e416f0330e978cb07ae5aafaeef
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-22 21:57:11 +0100

    feat(ui): termion complient colors

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -1,19 +1,36 @@
+use ansi_term::Colour::*;
 use termion::color::*;
 
-pub const SAND_COLOR: Rgb               = Rgb(207, 169, 138); 
-pub const LIGHT_COLOR: Rgb              = Rgb(205, 212, 165);
-pub const RUST_COLOR_1: Rgb             = Rgb(199, 120, 111);
-pub const RUST_COLOR_2: Rgb             = Rgb(154, 98, 120);
-pub const GREY_COLOR: Rgb               = Rgb(96, 85, 110);
-pub const GREY_BLUE_COLOR: Rgb          = Rgb(59, 64, 94);
-pub const DARK_COLOR: Rgb               = Rgb(46, 42, 79);
-pub const DARKEST_COLOR: Rgb            = Rgb(33,29, 56);
+pub const SAND_COLOR: Rgb = Rgb(207, 169, 138);
+pub const LIGHT_COLOR: Rgb = Rgb(205, 212, 165);
+pub const RUST_COLOR_1: Rgb = Rgb(199, 120, 111);
+pub const RUST_COLOR_2: Rgb = Rgb(154, 98, 120);
+pub const GREY_COLOR: Rgb = Rgb(96, 85, 110);
+pub const GREY_BLUE_COLOR: Rgb = Rgb(59, 64, 94);
+pub const DARK_COLOR: Rgb = Rgb(46, 42, 79);
+pub const DARKEST_COLOR: Rgb = Rgb(33, 29, 56);
 
 // UI
-pub const UI_BLACK_COLOR: Rgb           = Rgb(21,14, 16);
-pub const UI_BLACK_LIGHT_COLOR: Rgb     = Rgb(39,39, 57);
-pub const UI_BLACK_LIGHTER_COLOR: Rgb   = Rgb(57,56, 73);
+pub const UI_BLACK_COLOR: Rgb = Rgb(21, 14, 16);
+pub const UI_BLACK_LIGHT_COLOR: Rgb = Rgb(39, 39, 57);
+pub const UI_BLACK_LIGHTER_COLOR: Rgb = Rgb(57, 56, 73);
 
-pub const UI_WHITE_COLOR: Rgb           = Rgb(119,136, 140);
-pub const UI_WHITE_DARK_COLOR: Rgb      = Rgb(79,90, 100);
-pub const UI_WHITE_DARKER_COLOR: Rgb    = Rgb(59,65, 82);
+pub const UI_WHITE_COLOR: Rgb = Rgb(119, 136, 140);
+pub const UI_WHITE_DARK_COLOR: Rgb = Rgb(79, 90, 100);
+pub const UI_WHITE_DARKER_COLOR: Rgb = Rgb(59, 65, 82);
+
+pub const A_SAND_COLOR: ansi_term::Colour = RGB(207, 169, 138);
+pub const A_LIGHT_COLOR: ansi_term::Colour = RGB(205, 212, 165);
+pub const A_RUST_COLOR_1: ansi_term::Colour = RGB(199, 120, 111);
+pub const A_RUST_COLOR_2: ansi_term::Colour = RGB(154, 98, 120);
+pub const A_GREY_COLOR: ansi_term::Colour = RGB(96, 85, 110);
+pub const A_GREY_BLUE_COLOR: ansi_term::Colour = RGB(59, 64, 94);
+pub const A_DARK_COLOR: ansi_term::Colour = RGB(46, 42, 79);
+pub const A_DARKEST_COLOR: ansi_term::Colour = RGB(33, 29, 56);
+// GB
+pub const A_UI_BLACK_COLOR: ansi_term::Colour = RGB(21, 14, 16);
+pub const A_UI_BLACK_LIGHT_COLOR: ansi_term::Colour = RGB(39, 39, 57);
+pub const A_UI_BLACK_LIGHTER_COLOR: ansi_term::Colour = RGB(57, 56, 73);
+pub const A_UI_WHITE_COLOR: ansi_term::Colour = RGB(119, 136, 140);
+pub const A_UI_WHITE_DARK_COLOR: ansi_term::Colour = RGB(79, 90, 100);
+pub const A_UI_WHITE_DARKER_COLOR: ansi_term::Colour = RGB(59, 65, 82);
