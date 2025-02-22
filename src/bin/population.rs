814c38c83e5eecc285d386e17e419a18ae42ee85
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-22 22:23:11 +0100

    refactor(bin): testing update_population

--- src/bin/population.rs
+++ src/bin/population.rs
@@ -1,5 +1,6 @@
 use rustupolis::population::*;
-use rustupolis::population::district::*;
+// use rustupolis::population::district::*;
+use rustupolis::simulation::update_population;
 
 fn main() {
     // By default a popluation start with a core district.
@@ -8,15 +9,8 @@ fn main() {
     // add 50 peoples to Core district. (by default (None), it is the Core district)
     population.add_peoples(50, None); 
     
-    // add 80 people to the newly created slums
-    let slum_id = population.add_district(80, DistrictZone::Slums); 
-
-
-    let slums_neighbors = population.get_district_neighbors(slum_id).unwrap();
-    
-    // When a district is added, links will be automatically added between the new and old districts
-    let core_district = slums_neighbors[0];
-
-    dbg!(&core_district.peoples[0]);
-    println!("Core happiness level : {}%", core_district.happiness_percentage() * 100.0);
+    // // add 80 people to the newly created slums
+    // let slum_id = population.add_district(80, DistrictZone::Slums); 
+  
+    update_population(&mut population);
 }
