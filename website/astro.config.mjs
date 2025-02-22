e7771c68b00537d5afb02e4bbc0818ece58a7164
Bastien1533 <bastien.pinaud@yahoo.com>
2025-02-22 15:20:49 +0100

    fix(Website):  Remove alpine dep

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import alpinejs from '@astrojs/alpinejs';
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
 
   site: 'https://deadelineurz.github.io',
   base : 'rustupolis',
-  integrations: [sitemap(), alpinejs()]
+  integrations: [sitemap()]
 });
