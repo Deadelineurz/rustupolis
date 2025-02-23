9bd2bd7bd980bde631ea698edba8ae742ab6fe1f
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-02-24 00:49:58 +0100

    refactor(bin): remade population bin

--- src/bin/population.rs
+++ src/bin/population.rs
@@ -5,12 +5,19 @@ use rustupolis::simulation::update_population;
 fn main() {
     // By default a popluation start with a core district.
     let mut population = Population::new();
-    
+
     // add 50 peoples to Core district. (by default (None), it is the Core district)
-    population.add_peoples(50, None); 
-    
-    // // add 80 people to the newly created slums
-    // let slum_id = population.add_district(80, DistrictZone::Slums); 
-  
-    update_population(&mut population);
+    population.add_peoples(50, None);
+
+
+
+    for i in 1..=50 {
+        println!("_____Year {i}_____");
+
+        println!("Before = {}\n", population.get_core_district().num_people);
+
+        update_population(&mut population, None);
+
+        println!("After = {}\n", population.get_core_district().num_people);
+    }
 }
