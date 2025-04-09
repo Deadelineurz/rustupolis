ff49e08024b6b795e9f2dcf77c1c3b9191442cf0
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-04-09 15:13:12 +0200

    feat: Added top bar to display stuff

--- src/ui/topbar.rs
+++ src/ui/topbar.rs
@@ -3,10 +3,12 @@ use termion::terminal_size;
 use super::colors::*;
 use crate::engine::keybinds::Tty;
 use crate::terminal::boxes::*;
+use crate::terminal::lines::{draw_line, LineDirection, LineStyle};
+use crate::terminal::text::draw_text;
 use std::ops::Deref;
 use std::{io::Error, sync::Arc};
 
-const TOPBAR_HEIGHT_MULTIPLIER: u16 = 8;
+const TOPBAR_HEIGHT_MULTIPLIER: u16 = 10;
 
 pub struct TopBar {
     stdout: Arc<Tty>,
@@ -14,6 +16,7 @@ pub struct TopBar {
     ter_width: u16,
     ter_height: u16,
 
+    width: u16,
     height: u16,
 
     day_number: u32,
@@ -21,23 +24,28 @@ pub struct TopBar {
 
 impl TopBar {
     /// Will get the terminal size
-    pub fn new(stdout: Arc<Tty>) -> Self {
+    pub fn new(stdout: Arc<Tty>, right_width_offset: u16) -> Self {
         let (x, y) = terminal_size().unwrap();
-        let height = x / TOPBAR_HEIGHT_MULTIPLIER;
+        let width = x - right_width_offset - 1;
+        let height = y / TOPBAR_HEIGHT_MULTIPLIER;
 
         TopBar {
             stdout,
             hide: false,
             ter_width: x,
             ter_height: y,
-
+            width,
             height,
 
             day_number: 0,
         }
     }
 
-    pub fn next_day(mut self) {
+    pub fn get_height(&self) -> u16 {
+        self.height
+    }
+
+    pub fn next_day(&mut self) {
         self.day_number += 1;
     }
 
@@ -46,7 +54,62 @@ impl TopBar {
         self.ter_width = x;
         self.ter_height = y;
 
-        self.height = x / TOPBAR_HEIGHT_MULTIPLIER;
+        self.height = y / TOPBAR_HEIGHT_MULTIPLIER;
+    }
+
+    /// Update the population number on the topbar.
+    pub fn update_displayed_population(&self, amount: usize) -> Result<(), Error> {
+        draw_text(
+            &self.stdout,
+            &(amount.to_string().to_owned() + &" ".repeat(4)),
+            16,
+            3,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )
+    }
+
+    /// Update the year number on the topbar.  
+    /// \(Will add 2000 to the amount provided)
+    pub fn update_displayed_year(&self, amount: usize) -> Result<(), Error> {
+        draw_text(
+            &self.stdout,
+            &((2000 + amount).to_string().to_owned() + &" ".repeat(1)),
+            4,
+            3,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )
+    }
+
+    pub fn update_displayed_happiness(&self, percentage: f32) -> Result<(), Error> {
+        draw_text(
+            &self.stdout,
+            &(((percentage * 100.0) as u8).to_string().to_owned() + "%" + &" ".repeat(1)),
+            31,
+            3,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )
+    }
+
+    pub fn update_displayed_workers(&self, amount: u16, population: usize) -> Result<(), Error> {
+        draw_text(
+            &self.stdout,
+            &(amount.to_string().to_owned() + &" ".repeat(1)),
+            44,
+            3,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )?;
+        draw_text(
+            &self.stdout,
+            &(((amount as f32 / population as f32) as u8).to_string().to_owned() + "%" + &" ".repeat(1)),
+            44,
+            4,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )
     }
 
     pub fn draw(&self) -> Result<(), Error> {
@@ -58,11 +121,82 @@ impl TopBar {
             self.stdout.deref(),
             1,
             1,
-            self.ter_width + 1,
+            self.width,
             self.height + 1,
             BoxStyle::new()
                 .fill(BoxFill::color(UI_BLACK_COLOR))
                 .lines_color(UI_WHITE_COLOR),
+        )?;
+        draw_text(&self.stdout, "Year :", 3, 2, UI_WHITE_COLOR, UI_BLACK_COLOR)?;
+        draw_text(
+            &self.stdout,
+            &2000.to_string(),
+            4,
+            3,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )?;
+
+        draw_line(
+            &self.stdout,
+            10,
+            2,
+            3,
+            LineStyle::new().direction(LineDirection::Vertical),
+        )?;
+
+        draw_text(
+            &self.stdout,
+            "Population :",
+            12,
+            2,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )?;
+        draw_text(&self.stdout, "100", 16, 3, UI_WHITE_COLOR, UI_BLACK_COLOR)?;
+
+        draw_line(
+            &self.stdout,
+            25,
+            2,
+            3,
+            LineStyle::new().direction(LineDirection::Vertical),
+        )?;
+
+        draw_text(
+            &self.stdout,
+            "Happiness :",
+            27,
+            2,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )?;
+        draw_text(&self.stdout, "50%", 31, 3, UI_WHITE_COLOR, UI_BLACK_COLOR)?;
+
+        draw_line(
+            &self.stdout,
+            39,
+            2,
+            3,
+            LineStyle::new().direction(LineDirection::Vertical),
+        )?;
+
+        draw_text(
+            &self.stdout,
+            "Workers :",
+            41,
+            2,
+            UI_WHITE_COLOR,
+            UI_BLACK_COLOR,
+        )?;
+        draw_text(&self.stdout, "0", 44, 3, UI_WHITE_COLOR, UI_BLACK_COLOR)?;
+
+        draw_line(
+            &self.stdout,
+            51,
+            2,
+            3,
+            LineStyle::new().direction(LineDirection::Vertical),
         )
     }
 }
