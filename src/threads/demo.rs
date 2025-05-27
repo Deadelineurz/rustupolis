fda977f047940ea2e5228894d44f9b20383d38c0
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-05-27 15:03:34 +0200

    feat(demo): added lines to test the generation

--- src/threads/demo.rs
+++ src/threads/demo.rs
@@ -67,6 +67,14 @@ pub fn demo_scope<'scope, 'env>(
 
             lock_unlock!(pop);
 
+            if i % 36 == 0 {
+                lock_write!(engine |> generator);
+
+                generate_next_step(&mut generator.layout, &mut rng);
+
+                lock_unlock!(generator);
+            }
+
             if refresh == 20 {
                 lock_write!(engine |> e);
                 refresh = 0;
